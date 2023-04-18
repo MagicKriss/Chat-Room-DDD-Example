ca89826b775d120bb238fd6e8c0b7d0fb7855fa2
MagicKriss <krisjanis.kallings@gmail.com>
2023-04-18 18:42:10 +0300

    feat: Add Swagger/OpenAPI & versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
+import { INestApplication, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { GLOBAL_API_PREFIX } from './constants';
+
+function swaggerSetup(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Chat Room Example')
+    .setDescription('The Chat Room API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(GLOBAL_API_PREFIX, app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix(GLOBAL_API_PREFIX);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  swaggerSetup(app);
+
   await app.listen(3000);
 }
-bootstrap();
+
+/**
+ * BOOTSTRAP THE APP
+ */
+
+bootstrap()
+  .then(() => {
+    console.log('Application bootstraped successfully');
+  })
+  .catch((err) => {
+    console.error(`Application bootstrap failed: ${err.message}`);
+  });
